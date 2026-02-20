3cce9d5e5b00a385fcb8f1cca675160405cc2eb8
Dan Welling <dwelling@umich.edu>
2026-02-20 16:28:50 -0500

    Adding some initial material as a start/example

--- __init__.py
+++ __init__.py
@@ -5,4 +5,74 @@ A package for verifying modeled auroral precipitation against SSUSI
 observations.
 '''
 
-print('I work!')
+import numpy as np
+from spacepy.datamodel import dmarray, SpaceData
+from spacepy.pybats import rim
+from spacepy.plot import set_target
+
+
+class PrecipFile(SpaceData):
+    '''
+    Lots of docs here!
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super(PrecipFile, self).__init__(*args, **kwargs)  # Init as SpaceData.
+
+        self.attrs['nlon'] = 10
+        self.attrs['nlat'] = 10
+        self['avee'] = np.zeros(self.attrs['nlon'], self.attrs['nlat'])
+
+    def calc_hp(self):
+        '''
+        Calculate hemispheric power.
+        '''
+
+        pass
+
+        # self['ave'].sum()
+
+    def add_hemi_plot(self, target=None, loc=111):
+        '''
+        Add a hemispheric plot figure.
+
+        Parameters
+        ==========
+        obj : :class:`~spacepy.pybats.PbData` object
+           The object on which to act.
+
+        Other Parameters
+        ================
+
+        Returns
+        =======
+        fig : Matplotlib Figure object
+            The figure on which the plot is made.
+        ax : Matplotlib Axes object
+            The axes on which the plot is made.
+
+        Examples
+        ========
+        >>> import spacepy.pybats.bats as pbs
+        >>> mhd = pbs.Bats2d('spacepy-code/spacepy/pybats/slice2d_species.out')
+        >>> pbs._calc_ndens(mhd)
+        '''
+        pass
+
+
+class SwmfPrecip(PrecipFile):
+    '''
+    Subclass for handling swmf output.
+    '''
+
+    def __init__(self, filename, *args, **kwargs):
+        super(PrecipFile, self).__init__(*args, **kwargs)  # Init as SpaceData.
+
+        self.attrs['file'] = filename
+
+        data = rim.Iono(filename)
+
+        self['avee'] = data['n_ave']
+
+    def calc_hp(self):
+        pass
